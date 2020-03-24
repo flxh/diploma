815b7e6b4f189e1c5ab497bea8a8ddf747a2bb3b
Florus <fh_13@gmx.de>
2020-03-24 16:11:23 +0100

    Add setup.py

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='diploma',
+    version='',
+    packages=['ml', 'simulation', 'timeseriesprediction'],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='Florus Härtel',
+    author_email='',
+    description='',
+    install_requires=['tensorflow', 'numpy', 'scipy', 'scikit-learn', 'matplotlib', 'pandas']
+)
